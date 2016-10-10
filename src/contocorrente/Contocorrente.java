120e2ea15242d991be1644db0bce016be423a6ff
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-10-10 13:26:48 +0200

    Aggiornato progetto

--- src/contocorrente/Contocorrente.java
+++ src/contocorrente/Contocorrente.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Contocorrente {
 
-    private String      iban;
-    private float       saldo;
-    private ArrayList   movimenti;
-    private int         n_movimenti;
+    private String                  iban;
+    private float                   saldo;
+    private ArrayList<Movimento>    movimenti;
+    private int                     n_movimenti;
 
     public Contocorrente(String iban, float saldo) {
         this.iban           =   iban;
@@ -17,7 +17,7 @@ public class Contocorrente {
     }
 
     public void prelievo(int prelievo) {
-        if(prelievo > 0 && prelievo > this.saldo){
+        if(prelievo > 0 && prelievo < this.saldo){
            this. saldo -= prelievo;
            this.movimenti.add(new Movimento(++this.n_movimenti,System.currentTimeMillis(),prelievo,"prelievo"));
         }
@@ -37,5 +37,8 @@ public class Contocorrente {
     }
 
     public void printMovimenti() {
+        for(Movimento elem:this.movimenti){
+            System.out.println("Codice: "+elem.getCodice()+"\nData: "+elem.getData()+"\nImporto: "+elem.getImporto()+"\nCausale: "+elem.getCausale());
+        }
     }
 }

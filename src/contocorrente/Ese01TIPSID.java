1c5dfc4449b59a022742cddf09aef55fa1f43e73
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-11-13 16:49:07 +0100

    Aggiunti possibili casi di esecuzione

--- src/contocorrente/Ese01TIPSID.java
+++ src/contocorrente/Ese01TIPSID.java
@@ -10,14 +10,14 @@ public class Ese01TIPSID {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-        // TODO code application logic here
-        Contocorrente conto = new Contocorrente("iiosonoiban",500);
-        /*
-        conto.prelievo(20);
-        conto.versamento(500);
         
-        conto.printMovimenti();
+        Contocorrente conto = new Contocorrente("iiosonoiban",400);
+        
+        /*
+            Due correntisti, uno che preleva e l'altro che versa
+            senza errori
         */
+        /*
         Correntista uno = new Correntista("UNO","aaa",conto,"prelievo",100);
         Correntista due = new Correntista("DUE","bbb",conto,"versamento",200);
         
@@ -26,6 +26,47 @@ public class Ese01TIPSID {
         
         uno.join();
         due.join();
+        */
+        
+        //--------------------------------------------------------//
+        
+        /*
+            Due correntisti, uno che preleva e l'altro che versa
+            con errori
+        */
+        /*
+        Correntista uno = new Correntista("UNO","aaa",conto,"prelievo",700);
+        Correntista due = new Correntista("DUE","bbb",conto,"versamento",200);
+        
+        uno.start();
+        due.start();
+        
+        uno.join();
+        due.join();
+        */
+        
+        //--------------------------------------------------------//
+        
+        /*
+            Più concorrenti che prelevano e versano anche con saldo inferiore
+            per vedere come sono gestiti bene gli eventuali casi di errori
+        */
+       
+        Correntista uno = new Correntista("UNO","aaa",conto,"prelievo",700);
+        Correntista due = new Correntista("DUE","bbb",conto,"versamento",200);
+        Correntista tre = new Correntista("TRE","ccc",conto,"prelievo",500);
+        Correntista quattro = new Correntista("QUATTRO","ddd",conto,"prelievo",100);
+        
+        uno.start();
+        due.start();
+        tre.start();
+        quattro.start();
+        
+        uno.join();
+        due.join();
+        tre.join();
+        quattro.join();
+        
         
         conto.printMovimenti();
         System.out.println("Totale conto: "+conto.getSaldo());
